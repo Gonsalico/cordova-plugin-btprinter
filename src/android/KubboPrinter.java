f24e106ac8ef9a4d0f50a9b9be3d99f04ca8fe64
Gonzalo <gonzalo.paniagua@kubbo.io>
2020-10-28 10:01:13 +0100

    readme modified and added ip and port parameters

--- src/android/KubboPrinter.java
+++ src/android/KubboPrinter.java
@@ -22,23 +22,25 @@ public class KubboPrinter extends CordovaPlugin{
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("printTest")) {
             String pdfFileBase64 = args.getString(0);
-            print(callbackContext, pdfFileBase64);
+            String ipPrinter = args.getString(1);
+            String portPrinter = args.getString(2);
+            print(callbackContext, pdfFileBase64, ipPrinter, portPrinter);
             return true;
         }
         return false;
     }
     
-    void print(CallbackContext callbackContext, String pdfFileBase64) {
+    void print(CallbackContext callbackContext, String pdfFileBase64, String ipPrinter, String portPrinter) {
         try {
             Log.d(LOG_TAG, "STARTING PRINT");
             // The line below illustrates the default port 6101 for mobile printers 9100 is the default port number
             // for desktop and tabletop printers
-            DataOutputStream outToServer = new DataOutputStream(getSocket(callbackContext).getOutputStream() );
+            DataOutputStream outToServer = new DataOutputStream(getSocket(callbackContext, ipPrinter, portPrinter).getOutputStream() );
             //The data being sent in the lines below illustrate CPCL  one can change the data for the corresponding
             //language being used (ZPL, EPL)
             outToServer.writeBytes(pdfFileBase64);
             Log.d(LOG_TAG, "PROCESS FINISHED");
-            callbackContext.success("Se ha Imprimido correctamente");
+            callbackContext.success("Printed Succesfully");
         } catch (Exception ex) {
             String errMsg = ex.getMessage();
             Log.e(LOG_TAG, errMsg);
@@ -46,11 +48,11 @@ public class KubboPrinter extends CordovaPlugin{
         }
     }
 
-    Socket getSocket(CallbackContext callbackContext) {
+    Socket getSocket(CallbackContext callbackContext, String ipPrinter, String portPrinter) {
 
         try {
             if(Objects.isNull(this.clientSocket)) {
-                this.clientSocket = new Socket("192.168.000.100",9100);
+                this.clientSocket = new Socket(ipPrinter,Integer.parseInt(portPrinter));
             }
             if(this.clientSocket.isConnected()){
                 return this.clientSocket;
--- www/KubboPrinter.js
+++ www/KubboPrinter.js
@@ -1,12 +1,9 @@
 var exec = require('cordova/exec');
 
 var KPrinter = {
-    printTest: function (fnSuccess, fnError, pdfFileBase64) {
-        exec(fnSuccess, fnError, "KubboPrinter", "printTest", [pdfFileBase64]);
+    print: function (fnSuccess, fnError, pdfFileBase64, ipPrinter, portPrinter) {
+        exec(fnSuccess, fnError, "KubboPrinter", "printTest", [pdfFileBase64, ipPrinter, portPrinter]);
     }
-    // print: function (fnSuccess, fnError, ip, port, base64Text) {
-    //     exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [ip, port, base64Text]);
-    // }
 };
 
 module.exports = KPrinter;
