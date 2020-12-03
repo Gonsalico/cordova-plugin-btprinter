1f3f61ff1e50567a75ad9f062f5477fb9112ab00
Gonzalo <gonzalo.paniagua@kubbo.io>
2020-12-03 11:29:56 +0100

    Added disconnect to interface

--- www/KubboPrinter.js
+++ www/KubboPrinter.js
@@ -3,6 +3,9 @@ var exec = require('cordova/exec');
 var KPrinter = {
     print: function (fnSuccess, fnError, pdfFileBase64, ipPrinter, portPrinter) {
         exec(fnSuccess, fnError, "KubboPrinter", "print", [pdfFileBase64, ipPrinter, portPrinter]);
+    },
+    disconnect: function (fnSuccess, fnError) {
+        exec(fnSuccess, fnError, "KubboPrinter", "disconnect", []);
     }
 };
 
